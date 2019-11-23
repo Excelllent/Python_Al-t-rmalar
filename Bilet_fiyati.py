16e2acfa61ae2337e7d2b652579279cd3bc54929
Fırat.Yesilgöz <56675568+Excelllent@users.noreply.github.com>
2019-11-23 16:36:04 +0300

    Bilet Fiyatı

---
+++ Bilet_fiyati.py
@@ -0,0 +1,33 @@
+# Python_Al-t-rmalar
+#if-elif-else ile uzun bir çözüm
+
+print("Uçak-Ekonomi-200",
+      "Uçak-Business-300",
+      "Tren-1-80",
+      "Tren-2-120",
+      "Otobüs-*-90")
+vasıta=input("Vasıta giriniz : ").lower().strip()
+sınıf=input("Sınıf giriniz : ").lower().strip()
+
+if vasıta=="uçak":
+    if sınıf=="ekonomi":
+        print("Bilet fiyatı : 200 ")
+    elif sınıf=="business":
+        print(" Bilet fiyatı : 300 ")
+    else:
+        print("Geçersiz sınıf ")
+elif vasıta=="tren":
+    if sınıf=="1":
+        print("Bilet fiyatı :80 ")
+    elif sınıf=="2":
+        print("Bilet fiyatı : 120 ")
+    else:
+        print("Geçersiz sınıf ")
+elif vasıta=="otobüs":
+    if sınıf=="*":
+        print("Bilet fiyatı : 90 ")
+    else:
+        print("Geçersiz sınıf ")
+        
+else:
+    print("Geçersiz Taşıt ")
